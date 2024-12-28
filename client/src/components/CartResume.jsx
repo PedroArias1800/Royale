50dd3ff2a4ed4f9dd7d36ce6c76a1fd49e4eec91
PedroArias1800 <pedrosoftdev18@gmail.com>
2024-12-27 19:37:27 -0500

    Ws

--- client/src/components/CartResume.jsx
+++ client/src/components/CartResume.jsx
@@ -115,7 +115,7 @@ export const CartResume = ({ products }) => {
     // Construir el enlace a WhatsApp con un mensaje dinámico
     const phoneNumber = "50765623382"; // Reemplaza con el número de WhatsApp
     const whatsappMessage = encodeURIComponent(message); // Codificar mensaje
-    const whatsappURL = `https://wa.me/${phoneNumber}?text=${whatsappMessage}&app=com.whatsapp`;
+    const whatsappURL = `whatsapp://send?phone=${phoneNumber}?text=${whatsappMessage}`;
 
     // Redirigir al enlace de WhatsApp
     window.open(whatsappURL, "_blank");
