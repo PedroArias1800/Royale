ddda22c128b97d54e66c8fe30918a2c066a51c49
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-16 23:09:53 -0500

    FIX socket.io 3

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 5174, // Puerto para admin
     proxy: {
@@ -12,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
