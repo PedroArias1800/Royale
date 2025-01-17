c0b39b5e7aa7c28caf3a0d0d929e6c1c5b1598c8
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-16 23:13:29 -0500

    FIX socket.io admin 2

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = process.env.VITE_SERVER_URL || 'http://localhost:4001'
+const URL = 'http://localhost:4001'
 
 const instance = axios.create({
     baseURL: URL,
