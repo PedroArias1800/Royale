0f2f41012417d0fb04db9e38f3e455b1e43ea003
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-18 16:22:06 -0500

    FIX JWT Cookie token

--- server/libs/jwt.js
+++ server/libs/jwt.js
@@ -1,7 +1,7 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from "../config.js";
 
-export const createAccessToken = (payload) => {
+export const createAccessToken = (payload, res) => {
     return new Promise((resolve, reject) => {
         jwt.sign(
             payload,
@@ -11,9 +11,18 @@ export const createAccessToken = (payload) => {
             },
             (err, token) => {
                 if (err) reject(err);
-                resolve(token)
-            }
-        )
-    })
 
-}
+                // Enviar la cookie con el token
+                res.cookie('token', token, {
+                    httpOnly: true,      // Impide el acceso por JavaScript
+                    secure: process.env.NODE_ENV === 'production', // Asegura que solo se envíe por HTTPS en producción
+                    sameSite: 'Strict',  // Mejora la seguridad contra CSRF
+                    expires: new Date(Date.now() + 3600000), // 1 hora de expiración, ajusta según tus necesidades
+                    path: '/'            // Asegura que esté disponible en todo el sitio
+                });
+
+                resolve(token); // Devolver el token para otras operaciones si es necesario
+            }
+        );
+    });
+};
