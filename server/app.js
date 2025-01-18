063f2ac3dbe7c2aaea40bff7006c6854a05c8b7d
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-17 19:11:52 -0500

    FIX Domain SSL

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
 import { connectDB } from './db.js';
-import { FRONTEND_URL, ADMIN_URL, FRONTEND_URL_DOMAIN_CERTICICATE, FRONTEND_URL_DOMAIN_CERTICICATE_WWW } from './config.js';
+import { FRONTEND_URL, ADMIN_URL, FRONTEND_URL_DOMAIN_CERTIFICATE, FRONTEND_URL_DOMAIN_CERTIFICATE_WWW } from './config.js';
 import authRoutes from './routes/auth.routes.js';
 import bodyRoutes from './routes/body.routes.js';
 import brandRoutes from './routes/brand.routes.js';
@@ -25,7 +25,7 @@ connectDB();
 export const app = express();
 
 app.use(cors({
-    origin: [FRONTEND_URL, ADMIN_URL, FRONTEND_URL_DOMAIN_CERTICICATE, FRONTEND_URL_DOMAIN_CERTICICATE_WWW],
+    origin: [FRONTEND_URL, ADMIN_URL, FRONTEND_URL_DOMAIN_CERTIFICATE, FRONTEND_URL_DOMAIN_CERTIFICATE_WWW],
     credentials: true,
 }));
 app.use(morgan('dev'));
