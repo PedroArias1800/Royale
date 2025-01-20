bce3c619ee3dd12ec0221e3019fe0ca75c9388c2
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-19 23:27:46 -0500

    FIX Brands Order 3

--- server/controllers/brand.controller.js
+++ server/controllers/brand.controller.js
@@ -4,6 +4,8 @@ import Parfum from '../models/parfum.model.js'
 export const getAllBrands = async(req, res) => {
     try{
         const brands = await Brand.find()
+        .sort({brand_name: 1});
+
         res.json(brands)
     } catch (error) {
         res.status(500).json({ message: "Brand not Found" })
@@ -19,7 +21,8 @@ export const getBrands = async (req, res) => {
 
         const brand = await Brand.find()
             .skip(skip)
-            .limit(limit);
+            .limit(limit)
+            .sort({brand_name: 1});
 
         const total = await Brand.countDocuments();
         const totalPages = Math.ceil(total / limit);
@@ -58,7 +61,8 @@ export const getFilteredBrands = async (req, res) => {
 
         const brand = await Brand.find(query)
             .skip(skip)
-            .limit(limit);
+            .limit(limit)
+            .sort({brand_name: 1});
 
         const total = await Brand.countDocuments();
         const totalPages = Math.ceil(total / limit);
