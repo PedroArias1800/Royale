27ece2ab0eb7c8a37f4e1c2dfabd827f32e81ffd
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-01-23 16:14:03 -0500

    FIX AuthProvider

--- admin/src/context/AuthProvider.jsx
+++ admin/src/context/AuthProvider.jsx
@@ -83,9 +83,6 @@ export const AuthProvider = ({ children }) => {
     const signIn = async (user) => {
         try {
             const res = await postLoginRequest(user);
-            const cookie = Cookies.get()
-            localStorage.setItem("user", JSON.stringify(res.data));
-            localStorage.setItem("cookie", cookie.token);
             setUser(res.data)
             setIsAuthenticated(true)
         } catch (error) {
@@ -120,16 +117,11 @@ export const AuthProvider = ({ children }) => {
             const cookie = Cookies.get()
             if (cookie.token){
                 try{
-                    const res = await verifyTokenRequest(localStorage.getItem("cookie"))
-                    if (!res.data) {
-                        localStorage.removeItem("cookie");
-                        localStorage.removeItem("user");
-                        return setIsAuthenticated(false)
-                    } else {
-                        setIsAuthenticated(true)
-                        setUser(localStorage.getItem("user"))
-                    }
+                    const res = await verifyTokenRequest(cookie.token)
+                    if (!res.data) return setIsAuthenticated(false)
     
+                    setIsAuthenticated(true)
+                    setUser(res.data)
                 } catch(err) {
                     setIsAuthenticated(false)
                     setUser(null)
