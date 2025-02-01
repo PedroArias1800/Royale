42aa86927f543205d809829562b6386831b93233
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-02-01 12:31:31 -0500

    FIX tamaño de archivos

--- server/app.js
+++ server/app.js
@@ -30,10 +30,10 @@ app.use(cors({
     credentials: true,
 }));
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Rutas
 app.use(authRoutes);
