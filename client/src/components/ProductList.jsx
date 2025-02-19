35abeffd3d29737d5c13b6b9a0ad9354bef72435
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-02-19 16:11:06 -0500

    FIX Search Parfums

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -9,7 +9,7 @@ export const ProductList = ({ products }) => {
         products.Parfum.map((product, index) => {
           const selectedType = product.types.find((type) => type.ml === "100") || product.types[0];
           return (
-            <Card element={product} selectedType={selectedType} cardsRef={cardsRef} index={index} key={index} width100={'auto'}/>
+            <Card element={product} selectedType={selectedType} cardsRef={cardsRef} index={index} key={index} width100={'auto'} typeOfSale={selectedType?.types?.type_of_sale || 'Normal'}/>
           )
         })}
     </div>
