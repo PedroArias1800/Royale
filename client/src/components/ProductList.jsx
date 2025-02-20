78fefc8986136021ae0bc1a8797bb17925983a41
PedroArias1800 <pedrosoftdev18@gmail.com>
2025-02-19 19:14:01 -0500

    FIX Price in Search

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -3,11 +3,13 @@ import { Card } from "./Card";
 
 export const ProductList = ({ products }) => {
   const cardsRef = useRef([]);
+  console.log(products)
   return (
     <div className="product-list">
       {
         products.Parfum.map((product, index) => {
           const selectedType = product.types.find((type) => type.ml === "100") || product.types[0];
+          console.log(`Data del Tipo Seleccionado: ${selectedType}`)
           return (
             <Card element={product} selectedType={selectedType} cardsRef={cardsRef} index={index} key={index} width100={'auto'} typeOfSale={selectedType?.types?.type_of_sale || 'Normal'}/>
           )
